4c00870088507f26c2ca3c4a3734dae4a4aac5f2
Sofia Hnatiuk <sofia.hnatiuk46@gmail.com>
2025-01-07 13:00:50 +0200

    removed calc.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,10 +1,2 @@
 from django.test import SimpleTestCase
 
-from app import calc
-
-class CalcTests(SimpleTestCase):
-
-    def test_add_numbers(self):
-        res = calc.add(3, 2)
-
-        self.assertEqual(res, 5)
