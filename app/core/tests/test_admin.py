e69477ed997f4fd6e73a947705b4f330cd4fccee
Sofia Hnatiuk <sofia.hnatiuk46@gmail.com>
2025-01-12 00:30:00 +0200

    user api

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from django.test import Client
 
 class AdminSiteTests(TestCase):
-
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -19,7 +18,6 @@ class AdminSiteTests(TestCase):
             )
 
     def test_users_list(self):
-
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
@@ -27,14 +25,12 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.email)
 
     def test_edit_user_page(self):
-
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
-
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
